4f2221fce82edc9e7e25dac96fd56213cad419d3
Katherine Estrella <43222926+estrella2492@users.noreply.github.com>
2020-02-10 11:32:03 -0500

    Laboratorio Esis Taxi

---
+++ app/src/main/java/com/example/conductoresisestrella/App.java
@@ -0,0 +1,31 @@
+package com.example.conductoresisestrella;
+
+import android.app.Application;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+
+public class App extends Application {
+    private static Socket mSocket;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//Se crea un socket que apunta a la ruta que se muestra
+        try {
+            mSocket = IO.socket("http://estrellaesistaxi.eu-4.evennode.com/");
+        }
+        catch (Exception e){
+        }
+    }
+    public static Socket getSocket() {
+        return mSocket;
+    }
+
+    public static String id;
+    public static String getidcliente(){
+        return id;
+    }
+    public static void setidcliente(String myid){
+        id=myid;
+    }
+}
